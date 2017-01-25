a0a9d38196bb0763c95e7ec0913b29cd28e99008
Roy Riojas <royriojas@gmail.com>
2017-01-24 23:47:41 -0500

    REF: test setting env vars from the output function

--- bnr-config.js
+++ bnr-config.js
@@ -4,6 +4,19 @@ module.exports = function() {
       cmd: function(args) {
         return 'eslint lib/**/*.js test/**/*.js *.js ' + (args.fix ? '--fix' : '');
       }
-    }
+    },
+    test: {
+      cmd: function(args) {
+        return {
+          cmd: './test-cmd.sh',
+          env: {
+            DEBUG: args.dbg
+          }
+        };
+      },
+      env: {
+        DEBUG: false
+      }
+    },
   };
 };
---
+++ test-cmd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# enable bash fail-fast
+
+env
